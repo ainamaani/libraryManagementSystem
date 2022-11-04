5498213f6667cedfa8709109a29d099ddeab840d
Ainamaani Isaac <aina.isaac2002@gmail.com>
2022-11-04 19:31:24 +0300

    Created returned books Schema

---
+++ models/returned.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const returnedBooksSchema = new Schema({
+    bookTitle: {
+        type : String,
+        required : true
+    },
+    bookTime: {
+        type : String,
+        required : true
+    }, 
+    bookDate: {
+        type : Date,
+        required: true
+    },
+    user:{
+        type : String,
+        required : true
+    }
+},{timestamps:true})
+
+
+const ReturnedBooks = mongoose.model('ReturnedBook',returnedBooksSchema)
+module.exports = ReturnedBooks
