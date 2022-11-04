c77608f44a4381f3d44924cf20df17d26cfdb3f3
Ainamaani Isaac <aina.isaac2002@gmail.com>
2022-11-04 19:26:13 +0300

    Created booked books model

---
+++ models/bookedBooks.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const bookedBooksSchema = new Schema({
+    bookTitle: {
+        type : String,
+        required : true
+    },
+    bookTime: {
+        type : String,
+        required : true
+    }, 
+    bookDate: {
+        type : Date,
+        required: true
+    },
+    user:{
+        type : String,
+        required : true
+    }
+},{timestamps:true})
+
+
+const BorrowedBooks = mongoose.model('BorrowedBook',bookedBooksSchema)
+module.exports = BorrowedBooks
